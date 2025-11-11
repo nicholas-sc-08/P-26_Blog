df886db09b549cce9a19b1def1440abf11ecb5fd
nicholas-sc-08 <nicholassc.2008@gmail.com>
2025-11-11 17:10:50 -0300

    wip: cadastro blog

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,26 +5,8 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div>
       <Header />
-      <Carousel className="min-h-screen">
-        <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="">
-              <div className="p-1 w-full max-h-screen">
-                <Card className="">
-                  <CardContent className="flex max-h-screen aspect-square items-center justify-center p-6">
-                    <span>Artigo {index + 1}</span>
-                    <span className="text-1xl font-semibold">Lorem ipsum</span>
-                  </CardContent>
-                </Card>
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Footer() {
+
+    return (
+        <div className="bg-black min-h-screen">
+            <footer className="flex min-h-screen justify-around bg-red">
+                <div className="">
+                </div>
+            </footer>
+        </div>
+    );
+};
